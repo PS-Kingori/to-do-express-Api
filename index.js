074ac29a4cd959b013c7d4f617651ab433f5e61f
PS-Kingori <peterskingori@gmail.com>
2025-04-17 15:50:33 +0300

    updated controller and routers

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import userRouter from './routes/users.js';
 dotenv.config();
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.MONGODB_URI
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use('/users', userRouter);
 
 
 app.listen(PORT, ()=>{
-    console.log(`listening from port http://localhost:${PORT}`)
+    console.log(`listening from AtlasDB`)
 })
 
 
